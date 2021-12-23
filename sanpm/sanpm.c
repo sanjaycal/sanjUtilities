2ee0701c68d7204b5addbe30b6bac2c831480c5f
Sanjay <Sanjay.calgry@gmail.com>
2021-12-22 17:26:30 -0700

    Fixed every utility to work with sanpm

--- sanpm/sanpm.c
+++ sanpm/sanpm.c
@@ -1,11 +1,61 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 
 
-int main(int argc, char *argv){
+
+void update(int software){
+	if(software == 0){
+		system("sudo wget -O santext https://github.com/sanjaycal/sanjUtilities/blob/master/santext/santext?raw=true");
+		system("sudo chmod +x santext");
+		system("sudo mv santext /bin");
+	}
+	if(software == 1){
+		system("sudo wget -O sancat https://github.com/sanjaycal/sanjUtilities/blob/master/sancat/sancat?raw=true");
+		system("sudo chmod +x sancat");
+		system("sudo mv sancat /bin");
+	}
+	if(software == 2){
+		system("sudo wget -O sancopy https://github.com/sanjaycal/sanjUtilities/blob/master/sancopy/sancopy?raw=true");
+		system("sudo chmod +x sancopy");
+		system("sudo mv sancopy /bin");
+	}
+	if(software == 3){
+		system("sudo wget -O sandate https://github.com/sanjaycal/sanjUtilities/blob/master/sandate/sandate?raw=true");
+		system("sudo chmod +x sandate");
+		system("sudo mv sandate /bin");
+	}
+	if(software == 4){
+		system("sudo wget -O sanls https://github.com/sanjaycal/sanjUtilities/blob/master/sanls/sanls?raw=true");
+		system("sudo chmod +x sanls");
+		system("sudo mv sanls /bin");
+	}
+	if(software == 5){
+		system("sudo wget -O sanmove https://github.com/sanjaycal/sanjUtilities/blob/master/sanmove/sanmove?raw=true");
+		system("sudo chmod +x sanmove");
+		system("sudo mv sanmove /bin");
+	}
+	if(software == 6){
+		system("sudo wget -O sanremove https://github.com/sanjaycal/sanjUtilities/blob/master/sanremove/sanremove?raw=true");
+		system("sudo chmod +x sanremove");
+		system("sudo mv sanremove /bin");
+	}
+}
+
+
+
+int main(int argc, char *argv[]){
 	
 	if(argc==2){
-		system("ls");
+		if (strcmp(argv[1],"update")==0){
+			for (int i = 0; i < 7; i++)
+			{
+				update(i);
+			}
+			
+		}
 	}
+
 	return 0;
 }
